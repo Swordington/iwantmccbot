6a37b0a9fa7d1654dbf1d35317a9309d554f9e83
COMPUTE\quinn <quinn@bigbrother.group>
2020-11-27 11:40:22 -0600

    Add some more triggers

--- triggers.js
+++ triggers.js
@@ -1,8 +1,17 @@
+/**
+ * NOTES ON THE TRIGGERS SYSTEM:
+ * Any additons must be in all lowercase, as the system converts all text to lowercase
+ * As of now (11/27/20), the system checks for a 100% match, and regex is not supported. Yet.
+ */
+
 module.exports = [
     "how do i join mcc?", 
     "how do i get into mcc?",
     "mcc ip?",
     "how do i join mcc",
     "where is the mcc signup",
-    "how do I get picked for mcc"
+    "how do i get picked for mcc",
+    "how do i get picked for mcc?",
+    "where is the mcc ip?",
+    "where is the mcc ip"
 ]
